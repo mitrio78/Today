04104cecc1b6310c32ca02c82d8b3126358a891e
Dmitriy Grishechko <mitrio78@gmail.com>
2022-02-04 12:21:52 +0300

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Дмитрий Гришечко on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
